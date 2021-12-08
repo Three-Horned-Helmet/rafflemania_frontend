1c09c0b1609fcd642a0810643f85b3fc9782b1e3
HaakonA <haakonaaa@stud.ntnu.no>
2021-12-08 14:56:00 +0100

    countDownTimer

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Username from "../src/components/Username";
 import UserCounter from "../src/components/UserCounter";
+import CountDownGameStart from "../src/components/countDownGameStart";
 import Messages from "../src/components/Messages";
 import MessageInput from "../src/components/MessageInput";
 import React, { useState, useEffect } from "react";
@@ -8,7 +9,7 @@ import io from "socket.io-client";
 
 export default function Home() {
   const [socket, setSocket] = useState(null);
-  const [response, setResponse] = useState("")
+  const [response, setResponse] = useState("");
 
   useEffect(() => {
     const newSocket = io("http://192.168.0.2:5000");
@@ -16,25 +17,23 @@ export default function Home() {
     return () => newSocket.close();
   }, [setSocket]);
 
-  
-  
   return (
     <div>
       <Head>
         <title>Rafflemania</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      { socket ? (
+      {socket ? (
         <>
           <Username />
           <UserCounter count="0" />
+          <CountDownGameStart />
           <Messages socket={socket} />
           <MessageInput socket={socket} />
         </>
       ) : (
         <div>Not Connected</div>
-        )}
+      )}
     </div>
   );
 }
-
--- src/components/countDownGameStart.js
+++ src/components/countDownGameStart.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-function countDownGameStart(props) {
-  return <div>Raffle will start in {props.time} seconds</div>;
+function countDownGameStart() {
+  let countDownDate = new Date("Dec 08, 2021 16:37:52").getTime();
+
+  var myfunc = setInterval(function () {
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+  }, 1000);
+
+  return (
+    <div>
+      Raffle will start in {startTime.getTime() - new Date().getTime()} seconds
+    </div>
+  );
 }
 
 export default countDownGameStart;
